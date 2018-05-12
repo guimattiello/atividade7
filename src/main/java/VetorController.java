bce00cd6624e1c935776ab0d19ba73cbd5449a36
Guilherme Ricken Mattiello <guimat@Guilhermes-MacBook-Pro.local>
2018-05-12 20:28:57 -0300

    Teste vetor nulo passando

---
+++ src/main/java/VetorController.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author guimat
+ */
+class VetorController {
+    
+    public void validaVetor(double[] vetor) {
+        
+        if (vetor == null) {
+            throw new IllegalArgumentException("Vetor nulo");
+        }
+        
+    }
+    
+}
